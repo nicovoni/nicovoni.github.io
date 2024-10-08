80177f4f57a13726c613b1663d9ef4e12917b15a
nicovoni <49231283+nicovoni@users.noreply.github.com>
2024-10-08 19:32:35 +0200

    Update btnscript.js

--- btnscript.js
+++ btnscript.js
@@ -11,8 +11,9 @@ function btnprenota(button) {
 
     // Mostra il messaggio di conferma
     if (confirm(messaggio)) {
-        // Logica per prenotare (da implementare)
-        alert("Prenotazione effettuata per la " + slotOra + "a ora del " + slotData + ".");
+        // Reindirizza l'utente alla nuova pagina HTML per inserire lo username e scegliere cosa prenotare
+        var url = "prenotazione.html?slotOra=" + encodeURIComponent(slotOra) + "&slotData=" + encodeURIComponent(slotData);
+        window.location.href = url;
     } else {
         alert("Prenotazione annullata.");
     }
