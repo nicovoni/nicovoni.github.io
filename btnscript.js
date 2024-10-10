71c3a3f3b1273b6292f95f5c2ae7ca5a52d4e88e
nicovoni <49231283+nicovoni@users.noreply.github.com>
2024-10-10 21:43:49 +0200

    Update btnscript.js

--- btnscript.js
+++ btnscript.js
@@ -12,7 +12,7 @@ function btnprenota(button) {
     // Mostra il messaggio di conferma
     if (confirm(messaggio)) {
         // URL del deployment della tua applicazione web (modifica con il tuo URL reale)
-        var baseURL = "https://script.google.com/macros/s/AKfycbyJKjVLi6AevXuXTQ9d9Cw0lRzSDX7ukiNZzXZsegpn5UYj9JDTtETlDi8cETv6_1Fp/exec";
+        var baseURL = "https://script.google.com/macros/s/AKfycbykEPMmCixPumXzGt8oqUDny_ZqGDVETQ74rBZeWAzcQDrCXInFP1ZrQ9fzaN22pedN/exec";
 
         // Aggiungi i parametri alla URL
         var url = baseURL + "?slotOra=" + encodeURIComponent(slotOra) + "&slotData=" + encodeURIComponent(slotData);
