247cb85b918c6e16e85def2b826626baf4bf6baf
nicovoni <49231283+nicovoni@users.noreply.github.com>
2024-10-13 11:53:52 +0200

    Update btnscript.js

--- btnscript.js
+++ btnscript.js
@@ -12,7 +12,7 @@ function btnprenota(button) {
     // Mostra il messaggio di conferma
     if (confirm(messaggio)) {
         // URL del deployment della tua applicazione web (modifica con il tuo URL reale)
-        var baseURL = "https://script.google.com/a/macros/isufol.it/s/AKfycby-P1nlxLAApV7FwKUafqwHMRiKwJbhl6AZrQvBFMly8VZgFx3pgX_CwM66uAlMhVVJ/exec";
+        var baseURL = "https://script.google.com/a/macros/isufol.it/s/AKfycbwB8AByqAvlEo1_TDlq0kit2Zt2o4v1bILou1oAw_A61IMf_TsFtdeHnvxs3mV2ilci/exec";
 
         // Aggiungi i parametri alla URL
         var url = baseURL + "?slotOra=" + encodeURIComponent(slotOra) + "&slotData=" + encodeURIComponent(slotData);
