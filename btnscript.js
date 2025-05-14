226b758dbec429900e3b402b1e8df1678bc7c73f
nicovoni <49231283+nicovoni@users.noreply.github.com>
2025-05-14 09:36:10 +0200

    Update btnscript.js

--- btnscript.js
+++ btnscript.js
@@ -28,7 +28,7 @@
             // Mostra il messaggio di conferma
             if (confirm(messaggio)) {
                 // Validazione e costruzione dell'URL
-                const baseURL = "https://script.google.com/a/macros/isufol.it/s/AKfycbx9HYD7sA6RT9i381aitbi_DS-1GVkaLK4uy5uH1WDwTc0-z2Yi9F8hwS1u0Pqm0Imd/exec";
+                const baseURL = "https://script.google.com/a/macros/isufol.it/s/AKfycbymzszFF7W8f9XsKtB_I-QLFPtNLOj1sjhcS0cq7Qg5Wj0g7hVXibfRQac9w1fGUUw-/exec";
                 const url = buildSafeURL(baseURL, { slotOra, slotData });
 
                 // Reindirizza l'utente alla nuova pagina servita da Apps Script
